4c9c61c6423711ccf5cc06e3a25baa34061e1877
Sourav Chakraborty <souravzzz@yahoo.co.in>
2013-01-18 17:15:06 +0530

    handle various options

--- go.php
+++ go.php
@@ -1,25 +1,54 @@
 <?php
-define(URL, 'http://tour.golang.org/compile');
 
-if(isset($argv[1])) {
+$shortopts = "v:f:u:dh";
+$longopts = array("version:","filename:","url:","debug","help");
+$options = getopt($shortopts, $longopts);
+
+$url = 'http://tour.golang.org/compile';
+$version = 1;
+$debug = false;
+$helpMessage = "\tphp go.php <filename.go>\n\tphp go.php [-v1/2] [-d] [-h] -f <filename.go> \n";
+
+foreach($options as $key => $val) {
+    if($key == 'v') {
+        $version = $val;
+    } else if($key == 'f') {
+        $srcFileName = $val;         
+    } else if($key == 'd') {
+        $debug = true;
+    } else if($key == 'd') {
+        $url = $val;         
+    } else if($key == 'h') {
+        print $helpMessage;
+        exit(0);
+    }
+}
+
+if($argc == 2 && isset($argv[1])) {
     $srcFileName = $argv[1];
-} else {
+}
+
+if($srcFileName == '') {
     die('Please enter source file to compile.');
+}
+
+if($url == '') {
+    die('Please enter url to contact. If you are not sure, remove -u option.');
 }
 
 $srcFile = fopen($srcFileName, 'r') or die ("Source file $srcFileName could not be read.");
 $srcCode = file_get_contents($srcFileName) or die ("Source file $srcFileName could not be read.");
-$postDataPrefix = 'version=2&body=';
+$postDataPrefix = "version=$version&body=";
 $postData = $postDataPrefix.urlencode($srcCode);
 $headers = array('Content-Type: application/x-www-form-urlencoded; charset=UTF-8','Accept: application/json, text/javascript, */*; q=0.01');
 
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, URL);
+curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_POST, 1); 
 curl_setopt($ch, CURLOPT_POSTFIELDS, $postData); 
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-#curl_setopt($ch, CURLOPT_VERBOSE, 1); 
+curl_setopt($ch, CURLOPT_VERBOSE, $debug); 
 
 $result = curl_exec($ch);
 if(!curl_errno($ch)) {
